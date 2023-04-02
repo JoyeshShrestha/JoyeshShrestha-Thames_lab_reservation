126c225517a981d3c2e8de234933309187acda96
Joyesh Shrestha <sjoyesh2000@gmail.com>
2023-04-02 16:30:21 +0545

    making a backbone for the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./app.css"
+import img from "./img/2.png"
+import itlogo from "./img/itcenterlogo.png"
+import home from "./img/homeicon.png"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav className="navbar navbar-dark navcol shadow p-3  bg-body rounded">
+    <div className="container-fluid">
+    <a className="navbar-brand"><img src={itlogo} alt="" /></a>
+
+    <div className=" lab-title text-uppercase">
+      Lab Reservation
     </div>
+    <a className="navbar-brand"><img src={img} alt="" /></a>
+    
+  </div>
+
+</nav>
+
+<div className="container-fluid">
+  <div className="row">
+    <div className="col-2 border sidebar rounded">
+      <div className="container">
+        <div className="row  tabs my-4">
+          <div className="col-7 text-center">
+          Home 
+          </div>
+          <div className="col text-center ">
+          {/* <img src={home} alt="" /> */}
+          </div>
+        </div>
+        <div className="row mt-5 text-white ">
+          Home
+        </div>
+      </div>
+    </div>
+   
+    <div className="col border main rounded m-4">
+      <div className="container">
+        <div className="row">
+          
+          
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+    </>
   );
 }
 
