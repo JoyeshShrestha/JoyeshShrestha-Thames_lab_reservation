a24d7bd451a9842df2d0176fbbc2dacac518a709
Joyesh Shrestha <sjoyesh2000@gmail.com>
2023-04-03 12:41:39 +0545

    Second day changes by J

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
 
 import "./app.css"
-import img from "./img/2.png"
-import itlogo from "./img/itcenterlogo.png"
-import home from "./img/homeicon.png"
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Home from "./home"
+import BookNow from "./booknow"
+
 
 
 
 function App() {
   return (
-    <>
-    <nav className="navbar navbar-dark navcol shadow p-3  bg-body rounded">
-    <div className="container-fluid">
-    {/* <a className="navbar-brand"><img src={itlogo} alt="" /></a> */}
-
-    <div className=" lab-title text-lowercase">
-      <span className="lab">Lab</span> Reservation<span className="lab">.</span>
-    </div>
-    <a className="navbar-brand"><img src={img} alt="" /></a>
     
-  </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="booknow" element={<BookNow />} />
 
-</nav>
-
-<div className="container-fluid">
-  <div className="row">
-    <div className="col-2 border sidebar rounded">
-      <div className="container-fluid">
-        <div className="row  tabs my-4">
-          <div className="col-7 text-left">
-          Home 
-          </div>
-         
-        </div>
-        <div className="row tabs my-4 ">
-        <div className="col-7 text-left">
-          Book Now
-          </div>
-          
-        </div>
-      </div>
-    </div>
-   
-    <div className="col border main rounded m-4">
-      <div className="container">
-        <div className="row">
-          
-          
-        </div>
-        
-      </div>
-    </div>
-  </div>
-  
-</div>
-
-    </>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import img from "./img/2.png"
+import {Link} from "react-router-dom"
+// import itlogo from "./img/itcenterlogo.png"
+// import h from "./img/homeicon.png"
+import Day from './component/day'
+import "./app.css"
+function home() {
+  return (
+   <>
+    <nav className="navbar navbar-dark navcol shadow p-3  bg-body rounded">
+    <div className="container-fluid">
+    {/* <a className="navbar-brand"><img src={itlogo} alt="" /></a> */}
+
+    <div className=" lab-title text-lowercase">
+      <span className="lab">Lab</span> Reservation<span className="lab">.</span>
+    </div>
+    <a className="navbar-brand"><img src={img} alt="" /></a>
+    
+  </div>
+
+</nav>
+
+<div className="container-fluid">
+  <div className="row">
+    <div className="col-2 border sidebar rounded">
+      <div className="container-fluid">
+        <Link to="/"><div className="row  tabs my-4">
+          <div className="col-7 text-left">
+          Home 
+          </div>
+          
+        </div>
+        </Link>
+        <Link to="/booknow">
+        <div className="row tabs my-4 ">
+        <div className="col-7 text-left">
+          Book Now
+          </div>
+          
+        </div>
+        </Link>
+      </div>
+    </div>
+   
+    <div className="col border main rounded m-4">
+      <div className="container">
+        <div className="row mt-5">
+          
+          <Day />
+
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
+   </>
+  )
+}
+
+export default home
